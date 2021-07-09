a8bd1bbe4893df2212be02cb8b1e80a17d10e064
Johan Sellberg <johan.sellberg@afaforsakring.se>
2021-07-09 14:02:23 +0200

    Add stone definition

--- handofstones.py
+++ handofstones.py
@@ -1,6 +1,15 @@
 import numpy as np
 import classes.stoneimage as si
 
+class stone:
+    stoneImage = si.stoneImage(np.zeros((5,5,3), dtype=np.uint8),np.zeros((5,5,3), dtype=np.uint8),"NoImage",-1)
+    xloc = -1
+    yloc = -1
+
+    def __init__(self,stoneImg, x, y):
+        self.stoneImage = stoneImg
+        self.xloc = x
+        self.yloc = y
 
 class handOfStones(object):
     stones = []
@@ -8,6 +17,9 @@ class handOfStones(object):
     def __init__(self,stones):
         self.stones = stones
    
+    def sortByXlocation(self):
+        pass
+
     def print(self):
         rowindex = -1        
         for row in self.stones:
